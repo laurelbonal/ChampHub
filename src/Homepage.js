67225ddbaaf84c87b10821c96e1a39d4967ba09d
laurel Bonal <laurelbonal@gmail.com>
2024-05-31 13:07:01 -0600

    Add styling for homepage

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Homepage.css'
+
+export default function Homepage() {
+    return (
+        <div>
+            <h1 className="h11">find your new favorite</h1>
+            <h1 className="h12">CHAMPION</h1>
+            <p>filter by champ type and difficulty level to find the perfect match for your play style</p>
+            <button>let's get started</button>
+        </div>
+    );
+}
