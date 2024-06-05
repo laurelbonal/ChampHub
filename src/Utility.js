3161c2d01e0391c366fb1d831fb17377ea1c8c57
laurel Bonal <laurelbonal@gmail.com>
2024-06-04 21:09:56 -0600

    Add media query for small screen sizes

--- src/Utility.js
+++ src/Utility.js
@@ -1,13 +1,11 @@
 export default function formatChampionName(name) {
     let formattedName = name;
-    
-    // Handle apostrophes
+  
     if (name.includes("'")) {
         const formattedNameOne = name.split("'").join('');
         formattedName = formattedNameOne.charAt(0).toUpperCase() + formattedNameOne.substring(1).toLowerCase();
     }
 
-    // Handle spaces
     if (name.includes(' ')) {
         formattedName = formattedName.split(' ').join('');
     }
