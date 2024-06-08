6a39e461a7fe89b65ce4d01f5ccdb44b48024938
laurel Bonal <laurelbonal@gmail.com>
2024-06-08 14:50:07 -0600

    Finish cypress testing

--- cypress/e2e/AllChamps.cy.js
+++ cypress/e2e/AllChamps.cy.js
@@ -4,13 +4,32 @@ describe('template spec', () => {
       statusCode: 200,
       fixture: 'AllChamps'
     }).as('getChamps')
-    cy.visit('http://localhost:3000/Champions');
+    cy.visit('http://localhost:3000');
   });
   it('should display champions', ()=>{
     cy.wait('@getChamps')
     cy.get('.header-link > h1').contains('ChampionHub')
     cy.get('.sticky-container > h1').contains('All Champs')
     cy.get('.sticky-container > p').contains('Filter by champ type and difficulty level to find the perfect match for your play style')
-    cy.get('[aria-label="Type"]').contains
+    cy.get('[aria-label="Type"]').contains('Fighter')
+    cy.get('[aria-label="Type"]').contains('Mage')
+    cy.get('[aria-label="Type"]').contains('Assassin')
+    cy.get('[aria-label="Type"]').contains('Tank')
+    cy.get('[aria-label="Type"]').contains('Support')
+    cy.get('[aria-label="Difficulty"]').contains('1 - Easy')
+    cy.get('[aria-label="Difficulty"]').contains('2 - Moderate')
+    cy.get('[aria-label="Difficulty"]').contains('3 - Hard')
+    cy.get('[aria-label="Difficulty"]').contains('4 - Very Hard')
+    cy.get('[aria-label="Difficulty"]').contains('5 - Extreme')
+    cy.get('.champ-card').should('have.length', '15')
+    cy.get('.champ-grid > :nth-child(1)').contains('Aatrox')
+    cy.get('.champ-grid > :nth-child(15)').contains('Blitzcrank')
+    cy.get('[aria-label="Type"]').select('Fighter')
+    cy.get('.champ-card').should('have.length', '2')
+    cy.get('.search').type('Aa')
+    cy.get('.champ-card').should('have.length', 1)
+    cy.get('.champ-card').should('contain', 'Aatrox')
+    cy.get('.clear-button').click()
+    cy.get('.champ-card').should('have.length', '15')
   })
 })
