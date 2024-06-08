14d766f0bd090fa65055a475f2d7418c32a26111
laurel Bonal <laurelbonal@gmail.com>
2024-06-08 09:53:55 -0600

    Add proptypes and better error handling

--- src/Pages/AllChamps/ChampCard.js
+++ src/Pages/AllChamps/ChampCard.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useParams, useNavigate } from 'react-router-dom';
 import './ChampCard.css';
 import formatChampionName from '../../API-Utility/Utility';
 
 function ChampCard({ name, image, blurb }) {
-    const navigate = useNavigate();
-    const { name: paramName } = useParams();
+  const navigate = useNavigate();
+  const { name: paramName } = useParams();
 
-    const handleCardClick = () => {
-        const formattedName = formatChampionName(name);
-        navigate(`/Champion/${formattedName}/about`);
-    };
+  const handleCardClick = () => {
+    const formattedName = formatChampionName(name);
+    navigate(`/Champion/${formattedName}/about`);
+  };
 
-    return (
-        <div className="champ-card" onClick={handleCardClick}>
-            <img src={image} alt={name} className="champ-image" />
-            <h2 className="champ-name">{name}</h2>
-            <p className="champ-blurb">{blurb}</p>
-        </div>
-    );
+  return (
+    <div className="champ-card" onClick={handleCardClick}>
+      <img src={image} alt={name} className="champ-image" />
+      <h2 className="champ-name">{name}</h2>
+      <p className="champ-blurb">{blurb}</p>
+    </div>
+  );
 }
+
+ChampCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  blurb: PropTypes.string.isRequired
+};
 
 export default ChampCard;
